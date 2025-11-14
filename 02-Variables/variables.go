e4f4335bb00b23c86675861ef6017ef95b7c98cd
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2025-11-15 04:11:09 +0530

    adding 01-Hello-Go and 02-Variables files

---
+++ 02-Variables/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	// var name string = "Nitesh"
+
+	// infer
+	// var name = "Nitesh"
+
+	// var isAge = true
+
+	name := "Nitesh"
+	fmt.Println(name)
+}
