675fae04ccfa13dc91bad10116364f1e2ee48d45
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2026-05-07 02:00:58 +0530

    added array

---
+++ 8_array/array.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	// zeroed values,
+	// int ->0, string -> "",bool -> false
+	// var nums[5]int
+
+	// nums[0] =1 
+	// fmt.Println(nums[0])
+	// fmt.Println(nums)
+
+
+
+	// fmt.Println(len(nums))
+
+	// to declare in it single line
+	// nums := [3]int{1,2,3}
+
+	// fmt.Println(nums)
+
+	// 2D array
+	nums := [2][2]int{{3,4},{5,6}}
+	fmt.Println(nums)
+
+
+}
