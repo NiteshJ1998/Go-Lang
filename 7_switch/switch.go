bad30cec6338cc400648ba1470e0c1d4a2d28fe9
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2026-05-07 01:24:17 +0530

    added switch case

---
+++ 7_switch/switch.go
@@ -0,0 +1,19 @@
+package main
+
+func main(){
+	//simple switch
+
+	// i:= 3
+	// switch i {
+	// case 1:
+	// 	fmt.Println("one")
+	// case 2:
+	// 	fmt.Println("Two")
+	// case 3:
+	// 	fmt.Println("Three")
+	// default: 
+	// 	fmt.Println("other")
+	// }
+}
+
+
