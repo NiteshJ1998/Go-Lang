c9d3ba7ea39d224977b7ec1ba33cae5ad27e9242
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2026-05-06 23:03:46 +0530

    added ifelse

---
+++ 6_if_else/ifelse.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main(){
+	// age := 16
+
+	// if age>=18 {
+	// 	fmt.Println("person is an adult")
+	// }else{
+	// 	fmt.Println("person is not an adult")
+	// }
+
+	// age := 16
+
+	// if age >= 18 {
+	// 	fmt.Println("person is an adult ")
+	// }else if age>=12{
+	// 	fmt.Println("person is teenager")
+	// }else {
+	// 	fmt.Println("person is a kid")
+	// }
+
+	// var role = "admin"
+	// var haspermission = false
+
+	// if role == "admin" &&  haspermission {
+	// 	fmt.Println("this is block of code")
+	// }
+
+	// we can declare a variable inside a constructor
+	if age:= 14; age>= 18{
+		fmt.Println("person is not an adult", age)
+	}else if age >= 12{
+		fmt.Println("person is a teenager" ,age)
+	}
+
+}
